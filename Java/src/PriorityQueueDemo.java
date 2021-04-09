0a5bb236b56e14cb7335b36e41da46079cafb095
rramsdany <58965151+rramsdany@users.noreply.github.com>
2021-04-08 23:23:41 -0700

    Added Priority Queue.

---
+++ Java/src/PriorityQueueDemo.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+public class PriorityQueueDemo {
+
+    public static void main(String args[])
+    {
+        // Creating an empty PriorityQueue
+        PriorityQueue<String> queue = new PriorityQueue<String>();
+
+        // Use add() method to add elements into the Queue
+        queue.add("Welcome");
+        queue.add("To");
+        queue.add("Geeks");
+        queue.add("4");
+        queue.add("Geeks");
+
+        // Displaying the PriorityQueue
+        System.out.println("PriorityQueue: " + queue);
+    }
+}
